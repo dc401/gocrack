39b8d28af3020842459ce6e88083c3009597a4a3
SCIS Security <4706268+dc401@users.noreply.github.com>
2023-03-31 23:36:23 -0500

    Update main.go
    Returned this back to the single threaded edition. The multi threaded edition that splits the password wordlist file in half seems to not function properly unless the string is matched in both halves versus just one. I haven't figured it out yet and honestly the performance difference is negligible for this proof of concept learning

--- main.go
+++ main.go
@@ -6,19 +6,22 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
-	"strconv"
-	"sync"
-	"time"
+  "strconv"
+  "time"
 )
 
-/*
-Requires SHA2 with 256 bytes 64 rounds
-https://gchq.github.io/CyberChef/#recipe=SHA2('256',64,160)&input=SlVTVElOMTk5Mw
-JUSTIN1993 : 96306567b5683a463820ce53dd484eccc57c53aaa3c6d5f74018da1b8ef99815
+/* requires SHA2 with 256 bytes 64 rounds
+https://gchq.github.io/CyberChef/#recipe=SHA2('256',64,160)&input=Zm9vdGJhbGw
+echo football | sha256sum  produces -> 205b60ee79914af6a09b897170b522c5e16366214b9a0735b4eb550f4b14a3c8
+
+TL;DR - Basically, SHA1-256 mode vs. SHA2-256 mode
 */
+var checkHash string = "6382deaf1f5dc6e792b76db4a4a7bf2ba468884e000b25e7928e621e27fb23cb"
 
-var checkHash string = "96306567b5683a463820ce53dd484eccc57c53aaa3c6d5f74018da1b8ef99815"
-
+/* 
+wrap the hash creating objects into a function
+function definitions require return at the top
+*/
 func hashString(inputString string) string {
 	hashObject := sha256.New()
 	hashObject.Write([]byte(inputString))
@@ -26,66 +29,43 @@ func hashString(inputString string) string {
 	return hex.EncodeToString(calculatedHash)
 }
 
-// count lines in file to split between workers
-func countLines(filename string) int {
-	fileHandle, err := os.Open(filename)
+func main() {
+  /*
+  https://yourbasic.org/golang/measure-execution-time/
+  Found this sweet time measurement benchmark
+  */
+  startTime := time.Now()
+  
+	fmt.Println("Trying Hash: " + checkHash)
+
+  //open file and defer until main returns
+	fileHandle, err := os.Open("rockyou-full.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer fileHandle.Close()
-	counterInt := 0
+  
+  //set a counter just to see which line in your list works
+  counterInt := 0
+  
+  //create scanner object and iterate foo
 	scanner := bufio.NewScanner(fileHandle)
 	for scanner.Scan() {
-		counterInt++
-	}
-	return counterInt
-}
-
-func crackRange(minline int, maxline int, filename string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fileHandle, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer fileHandle.Close()
-	counterInt := minline
-	scanner := bufio.NewScanner(fileHandle)
-	//line := "" //must define variable since for loop causes scope issues
-	for counterInt <= maxline {
-		for scanner.Scan() {
-			line := scanner.Text()
-			counterInt++
-			hashedEntry := hashString(line)
-			if hashedEntry == checkHash {
-				foundLineNum := strconv.Itoa(counterInt)
-				fmt.Println("Found Password on line " + "(" + foundLineNum + "): " + line)
-				return
-			}
+		line := scanner.Text()
+    counterInt++
+    //you can use := vs define var foo type format inside a function struct
+		hashedEntry := hashString(line)
+		//fmt.Println("Entry: " + line +" : " + hashedEntry )
+		if hashedEntry == checkHash {
+      foundLineNum := strconv.Itoa(counterInt)
+			fmt.Println("Found Password on line " +"("+ foundLineNum +"): " +line)
+      //exits iteration subroutine back to main
+			break 
 		}
+
 	}
-}
-
-func main() {
-	startTime := time.Now()
-	fmt.Println("Trying Hash: " + checkHash)
-	//count total lines and split between worker jobs
-	totalCount := countLines("rockyou-odd.txt")
-	halfCount := totalCount / 2
-	fmt.Println("File line count: " + strconv.Itoa(halfCount))
-	fmt.Println("File half line count: " + strconv.Itoa(halfCount))
-
-	//use a WaitGroup to ensure all goroutines complete before ending the program
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go crackRange(0, halfCount, "rockyou-odd.txt", &wg)
-	go crackRange(halfCount, totalCount, "rockyou-odd.txt", &wg)
-
-	//wait for all goroutines to complete
-	wg.Wait()
-
-	//print out the nanoseconds spent
-	duration := time.Since(startTime)
-	fmt.Println("Runtime Nanoseconds: ")
-	fmt.Println(duration.Nanoseconds())
+  //print out the nanoseconds spent
+  duration := time.Since(startTime)
+  fmt.Println("Runtime Nanoseconds: ")
+  fmt.Println(duration.Nanoseconds())
 }
